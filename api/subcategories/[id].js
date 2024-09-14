ea5b14f3c77bad171963987323991381ebb06193
wael mohammed aly <wmaa64@yahoo.com>
2024-09-14 15:40:54 +0300

    Fix CORS and Place of dbConnect

--- api/subcategories/[id].js
+++ api/subcategories/[id].js
@@ -1,5 +1,4 @@
 import dbConnect from '../../utils/dbConnect';
-import Category from '../../models/Category';
 
 export default async function handler(req, res) {
   await dbConnect();
--- api/subcategories/index.js
+++ api/subcategories/index.js
@@ -1,5 +1,5 @@
 import dbConnect from '../../utils/dbConnect';
-import Category from '../../models/Category';
+
 
 export default async function handler(req, res) {
   await dbConnect();
