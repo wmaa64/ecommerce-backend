9fb55ea8cfe566de1b8f0f19dbffaa5f84ec1975
wael mohammed aly <wmaa64@yahoo.com>
2024-09-17 16:54:24 +0300

    at 17-09-2024

--- server.js
+++ server.js
@@ -22,18 +22,18 @@ app.use(cors({
   }));
 */
 
-//app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan('dev'));
 
 //Routes 
-/*
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories',categoryRoutes );
 app.use('/api/subcategories', subcategoryRoutes);
-*/
+
 
 /*
 mongoose.connect('mongodb://localhost:27017/ecommerce', {}).then(() => {
