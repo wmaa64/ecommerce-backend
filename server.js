adc072a86b3c784c0b4ae911af843a124fce7b51
wael mohammed aly <wmaa64@yahoo.com>
2024-09-18 20:43:10 +0300

    first today

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
-//import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
@@ -13,33 +12,17 @@ dotenv.config();
 
 const app = express();
 
-// Enable CORS for your frontend domain
-/*
-app.use(cors({
-    origin: '*',  // Allow any origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
-  }));
-*/
-
 app.use(cors());
 
 app.use(express.json());
 app.use(morgan('dev'));
 
 //Routes 
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories',categoryRoutes );
 app.use('/api/subcategories', subcategoryRoutes);
 
-
-/*
-mongoose.connect('mongodb://localhost:27017/ecommerce', {}).then(() => {
-    console.log('Database is Connected');
-}).catch(err => console.log(err));
-*/
 
 const dbURI = process.env.MONGODB_URI;
 
